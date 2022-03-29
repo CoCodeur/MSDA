a3690c19827956182c4160227de6fb2457dcb0a2
Corentin Devrouete <corentindevrouete.pro@gmail.com>
2022-03-29 12:08:56 +0200

    debig

--- test/add/add_test.c
+++ test/add/add_test.c
@@ -6,14 +6,9 @@
 #include "../../include/add.h"
 
 void add_test(void **state){
-<<<<<<< HEAD
      assert_true(add(1,2) == 3);
 }
 void add_test_2(void **state){
      assert_true(add(1,21) == 3);
 }
 
-=======
-     assert_true(add(1,1) == 3);
-}
->>>>>>> 50c973c244733e812e3ae321d6d69dab269a7b8e
