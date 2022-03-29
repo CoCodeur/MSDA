fbbad7e7d0724307959d56c9d5a4dfa7cb43d54c
Corentin Devrouete <corentindevrouete.pro@gmail.com>
2022-03-29 16:48:50 +0200

    change deprecated functions test

--- test/heap/heap_test.c
+++ test/heap/heap_test.c
@@ -9,12 +9,9 @@ static void test_init()
     assert_int_equal(1, 1);
 }
 
-int main(int argc, char **argv)
-{
-    const UnitTest tests[] =
-    {
-            unit_test(test_init),
-    };
-
-    return run_tests(tests);
+int main(){
+    const struct CMUnitTest tests[] = {
+                cmocka_unit_test(test_init),
+        };
+    return cmocka_run_group_tests(tests, NULL, NULL);
 }
