24ae31d10d001172d8bd71f557c3c7f4a4ae048b
Devrouete Corentin <50667329+CoCodeur@users.noreply.github.com>
2022-04-12 10:25:45 +0200

    Update main.c

--- src/main.c
+++ src/main.c
@@ -10,24 +10,5 @@
 
 int main(){
 
-   struct LINKEDLIST * linkedlist = linkedlist__new_linkedlist();
-
-   linkedlist__insert_head(linkedlist, 1);
-   linkedlist__insert_head(linkedlist, 2);
-   linkedlist__insert_head(linkedlist, 3);
-   linkedlist__insert_head(linkedlist, 4);
-   linkedlist__insert_head(linkedlist, 5);
-   linkedlist__insert_head(linkedlist, 6);
-   linkedlist__insert_head(linkedlist, 7);
-
-   
-
-   linkedlist__destroy(&linkedlist);
-
-   printf("%p", linkedlist);
-
-
-
-
    return 0;
 }
